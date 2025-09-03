5bb0d50e0cdd1ddac1a5c1e275d1367cc60c5049
jcuribevargas <vargasjeancarlos@gmail.com>
2025-09-03 17:38:48 -0500

    creacion de productController.js

---
+++ src/controllers/productController.js
@@ -0,0 +1,58 @@
+import prisma from "../prismaClient.js"; 
+
+// Crear un nuevo producto
+
+export const createProduct = async (req, res) => { 
+const { id, reference,screen, storage, ram, image, stock, priceCost, priceSale } = req.body;
+try { 
+const newProduct = await prisma.producto.create({ 
+ data: { id, reference,screen, storage, ram, image, stock, priceCost, priceSale }, 
+    }); 
+    res.json(newProduct); 
+  } catch (error) { 
+    res.status(400).json({ error: error.message });
+      } 
+};
+
+
+// Obtener todos los productos
+
+export const getProducts = async (req, res) => { 
+  try { 
+    const products = await prisma.task.findMany(); 
+    res.json(products); 
+  } catch (error) { 
+    res.status(500).json({ error: error.message }); 
+    console.error("Error al obtener los productos:", error); 
+  } 
+}; 
+ 
+// Actualizar un producto existente
+
+export const updateProduct = async (req, res) => {
+const { id } = req.params; 
+const { reference, screen, storage, ram, image, stock, priceCost, priceSale } = req.body;
+try { 
+const product = await prisma.producto.update({ 
+ where: { id: parseInt(id) }, 
+ data: { reference, screen, storage, ram, image, stock, priceCost, priceSale }, 
+    }); 
+    res.json(product); 
+  } catch (error) { 
+    res.status(400).json({ error: error.message }); 
+  }  
+}       
+
+// Eliminar un producto
+
+export const deleteProduct = async (req, res) => {
+const { id } = req.params; 
+try { 
+const product = await prisma.producto.delete({ 
+ where: { id: parseInt(id) }, 
+    }); 
+    res.json(product); 
+  } catch (error) { 
+    res.status(400).json({ error: error.message }); 
+  }
+};
