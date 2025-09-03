c21549e84598813dc820185fcc186165e2d27dcc
jcuribevargas <vargasjeancarlos@gmail.com>
2025-09-03 17:52:30 -0500

    actualiacion server para utilizar productRotes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from "express"; 
 import cors from "cors"; 
 import dotenv from "dotenv"; 
-import taskRoutes from "./routes/taskRoutes.js"; 
+import productRoutes from "./routes/productRoutes.js"; 
 import { connect } from "./prismaClient.js"; 
 dotenv.config();
 
 const app = express(); 
 app.use(cors()); // Permite peticiones desde otros orígenes (ej: frontend) 
 app.use(express.json()); // Permite recibir JSON desde el frontend 
-app.use("/api", taskRoutes); // Prefijo para nuestras rutas 
+app.use("/api", productRoutes); // Prefijo para nuestras rutas 
 // conectarme a la base de datos 
 connect(); 
 const PORT = process.env.PORT || 3001; 
